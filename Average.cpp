1227eb7edf7dadd8cf6e7690215826b48368a739
dong <variable9820@daum.net>
2024-11-05 19:18:39 +0900

    Average 함수 추가

---
+++ Average.cpp
@@ -0,0 +1,14 @@
+#include "OSAP_002_T6_source.h"
+double AvlTree::Average(node* current_node){ //노드 𝑥가 루트인 부분트리에서 노드들의 key 값의 최솟값(𝑎)와 최댓값(𝑏)의 산술평균((𝑎 + 𝑏)/2)을 출력한다.
+  double sumofleastandmost=0;
+  while(!current_node->left == NULL){
+    current_node = current_node->left;
+  }
+  sumofleastandmost += current_node->data;
+  current_node = head;
+  while(!current_node->right == NULL){
+    current_node = current_node->right;
+  }
+  sumofleastandmost += current_node->data;
+  return sumofleastandmost/2;
+}
