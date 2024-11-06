f9c6205f199872fe9dab15a1633da4e912c3b2b6
simhyunmin <shm66371945@gmail.com>
2024-11-07 02:27:44 +0900

    RankSearch.cpp 추가, Height 수정(스타일 가이드, 반환타입), InorderTraversalByValue 삭제, 헤더파일 수정

--- Rank.cpp
+++ Rank.cpp
@@ -2,11 +2,11 @@
 
 void AvlTree::Rank(int x){
   Node* node = NodeFindByValue(root, x);
-  if(node == nullptr){
+  if (node == nullptr) {
     cout << 0 << endl;
   }
   int depth_plus_height_sum = node -> depth + node -> height;
-  int node_rank = InorderTraversalByValue(root, x);
+  int node_rank = RankSearch(x);
 
-  cout << depth_plus_height_sum + node_rank << endl;
+  cout << depth_plus_height_sum + " " + node_rank << endl;
 }
