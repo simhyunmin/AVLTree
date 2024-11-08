e66fdd95b49a4d384af705027160cb42f50524a4
seaweedtreepot <seaweedtreepot@inha.edu>
2024-11-09 00:21:24 +0900

    Average 수정

--- Average.cpp
+++ Average.cpp
@@ -1,14 +1,18 @@
-// #include "OSAP_002_T6_source.h"
-// double AvlTree::Average(node* current_node){ //노드 𝑥가 루트인 부분트리에서 노드들의 key 값의 최솟값(𝑎)와 최댓값(𝑏)의 산술평균((𝑎 + 𝑏)/2)을 출력한다.
-//   double sumofleastandmost=0;
-//   while(!current_node->left == NULL){
-//     current_node = current_node->left;
-//   }
-//   sumofleastandmost += current_node->data;
-//   current_node = root;
-//   while(!current_node->right == NULL){
-//     current_node = current_node->right;
-//   }
-//   sumofleastandmost += current_node->data;
-//   return sumofleastandmost/2;
-// }
+#include "OSAP_002_T6_source.h"
+
+//노드 𝑥가 루트인 부분트리에서 노드들의 key 값의 최솟값(𝑎)와 최댓값(𝑏)의 산술평균((𝑎 + 𝑏)/2)을 출력한다.
+double AvlTree::Average(int x) { 
+  Node* sub_tree_root = NodeFindByValue(root, x);
+  
+  Node* min_node = sub_tree_root;
+  while (min_node -> left != NULL) {
+    min_node = min_node -> left;
+  }
+  
+  Node* max_node = sub_tree_root;
+  while (max_node -> right != NULL) {
+    max_node = max_node -> right;
+  }
+  
+  return static_cast<double>(min_node->data + max_node->data) / 2;
+}
