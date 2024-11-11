2716a6481a6703d3d6ee93048187f855f26033c5
simhyunmin <shm66371945@gmail.com>
2024-11-11 23:29:16 +0900

    정적 클래스 수정 및 시간 복잡도 개선 및 선언 일부 수정, main 쪽 while문 내부 코드 수정

--- Average.cpp
+++ Average.cpp
@@ -1,18 +1,18 @@
-#include "OSAP_002_T6_source.h"
+// #include "OSAP_002_T6_source.h"
 
-//노드 𝑥가 루트인 부분트리에서 노드들의 key 값의 최솟값(𝑎)와 최댓값(𝑏)의 산술평균((𝑎 + 𝑏)/2)을 출력한다.
-double AvlTree::Average(int x) { 
-  Node* sub_tree_root = NodeFindByValue(root, x);
+// //노드 𝑥가 루트인 부분트리에서 노드들의 key 값의 최솟값(𝑎)와 최댓값(𝑏)의 산술평균((𝑎 + 𝑏)/2)을 출력한다.
+// double AvlTree::Average(int x) { 
+//   Node* sub_tree_root = NodeFindByValue(root, x);
 
-  Node* min_node = sub_tree_root;
-  while (min_node -> left != NULL) {
-    min_node = min_node -> left;
-  }
+//   Node* min_node = sub_tree_root;
+//   while (min_node -> left != NULL) {
+//     min_node = min_node -> left;
+//   }
   
-  Node* max_node = sub_tree_root;
-  while (max_node -> right != NULL) {
-    max_node = max_node -> right;
-  }
+//   Node* max_node = sub_tree_root;
+//   while (max_node -> right != NULL) {
+//     max_node = max_node -> right;
+//   }
 
-  return static_cast<double>(min_node->data + max_node->data) / 2;
-}
+//   return static_cast<double>(min_node->data + max_node->data) / 2;
+// }
--- Find.cpp
+++ Find.cpp
@@ -1,6 +1,6 @@
-#include "OSAP_002_T6_source.h"
+// #include "OSAP_002_T6_source.h"
 
-void AvlTree::Find(int x) {
-  Node *node = NodeFindByValue(root, x);
-  cout << ((node != nullptr) ? node->depth + node->height : 0) << "\n";
-}
+// void AvlTree::Find(int x) {
+//   Node *node = NodeFindByValue(root, x);
+//   cout << ((node != nullptr) ? node->depth + node->height : 0) << "\n";
+// }
