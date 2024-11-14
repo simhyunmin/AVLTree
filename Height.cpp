59b16f78a9af498507c805590a15b598b400458a
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2024-11-14 18:47:40 +0900

    Height.cpp 수정
    기존 Height는 멤버함수를 참조했기 때문에 수정했습니다.

--- Height.cpp
+++ Height.cpp
@@ -1,9 +1,11 @@
 // #include "OSAP_002_T6_source.h"
 
-// void AvlTree::Height() const{
+// void AvlTree::Height() const {
 //   if (root == nullptr) {
-//     cout << -1 << endl;
-//   } else {
-//     cout << root -> height << endl;
+//     cout << -1 << "\n";
 //   }
-// }    
+//   else {
+//     cout << AvlTreeUtils::CalculateHeight(root) << endl;
+//   }
+// }
+   
